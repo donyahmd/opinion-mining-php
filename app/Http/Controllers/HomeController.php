f8efe6e95da9f097dac3890e2f083cff31b10947
Doni Ahmad <donyahmd24@gmail.com>
2020-03-09 19:02:15 +0800

    new data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,8 +29,8 @@ class HomeController extends Controller
         $positif = KlasifikasiKomentar::where('klasifikasi', 'positif')->count();
         $negatif = KlasifikasiKomentar::where('klasifikasi', 'negatif')->count();
 
-        $persentase_positif = number_format($positif / $total * 100, 2);
-        $persentase_negatif = number_format($negatif / $total * 100, 2);
+        $persentase_positif = $total == 0 ? 50 : number_format($positif / $total * 100, 2);
+        $persentase_negatif = $total == 0 ? 50 : number_format($negatif / $total * 100, 2);
 
         return view('backend.beranda.index', compact('persentase_positif', 'persentase_negatif'));
     }
