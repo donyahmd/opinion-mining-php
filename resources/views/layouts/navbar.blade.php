12323b3bb8b0e44fdec121abd225b57eafaff0d7
Doni Ahmad <donyahmd24@gmail.com>
2020-04-07 23:04:02 +0800

    logout form working

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -65,7 +65,8 @@
                             <a href="#" class="btn btn-default btn-flat">Profile</a>
                         </div>
                         <div class="pull-right">
-                            <a href="" class="btn btn-default btn-flat">Sign out</a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
+                            <a href="#" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="btn btn-default btn-flat">Logout</a>
                         </div>
                     </li>
                 </ul>
